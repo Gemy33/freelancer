40ef5a8176f7a50dbc5df0f1a315da8eab9171b5
IslamFathyAbdeltwab <156615484+IslamFathyAbdeltwab@users.noreply.github.com>
2024-10-05 17:33:20 +0300

    footer design

--- cafe/freejob/src/app/app.component.ts
+++ cafe/freejob/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { NavbarComponent } from "./component/navbar/navbar.component";
+import { FooterComponent } from "./component/footer/footer.component";
 
 
 
@@ -10,7 +11,7 @@ import { NavbarComponent } from "./component/navbar/navbar.component";
   standalone: true,
   
 
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
 
 
 
