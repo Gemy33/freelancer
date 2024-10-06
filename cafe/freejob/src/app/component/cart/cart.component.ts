ebc9a887934db3a79b1e19f6062eaef55b5fd067
Gemy33 <mohamedgamalabdelhaliym@gmail.com>
2024-10-06 11:55:55 +0300

    end the cart design

--- cafe/freejob/src/app/component/cart/cart.component.ts
+++ cafe/freejob/src/app/component/cart/cart.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { BgFixedComponent } from "../reuseable-components/bg-fixed/bg-fixed.component";
+import { FixedInfoComponent } from "../reuseable-components/fixed-info/fixed-info.component";
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [BgFixedComponent, FixedInfoComponent],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss'
 })
